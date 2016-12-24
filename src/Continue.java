e0ef4c82e43313e9ba2bba779710a18ef8e87b04
chenchunwei24k@gmail.com <chenchunwei24k@gmail.com>
2016-12-24 11:43:51 +0800

    HW2-2

---
+++ src/Continue.java
@@ -0,0 +1,13 @@
+
+public class Continue {
+
+	public static void main(String[] args) {
+		for(int i=1; i<5; i++){
+			if(i==0 || i==2 || i==4)	continue;
+			System.out.println(i);
+		}
+		
+
+	}
+
+}
